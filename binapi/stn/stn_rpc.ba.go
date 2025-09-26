2d572915a943378987d748218fa36a8116f17dfa
Matz von Finckenstein <176327046+mvonfinckenstein@users.noreply.github.com>
2025-09-26 16:37:32 -0400

    v25.06 generated bindings

--- binapi/stn/stn_rpc.ba.go
+++ binapi/stn/stn_rpc.ba.go
@@ -7,8 +7,8 @@ import (
 	"fmt"
 	"io"
 
+	memclnt "github.com/equinixmetal/govpp/binapi/memclnt"
 	api "go.fd.io/govpp/api"
-	memclnt "go.fd.io/govpp/binapi/memclnt"
 )
 
 // RPCService defines RPC service stn.
